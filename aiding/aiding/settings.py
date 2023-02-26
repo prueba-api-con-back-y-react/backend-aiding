2411eb5c820cf55fe9999426cb68f03bdfb559ed
José María Delgado Sánchez <74303153+Josdelsan@users.noreply.github.com>
2023-02-26 21:36:00 +0100

    preuba2

--- aiding/aiding/settings.py
+++ aiding/aiding/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysql',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'test',
+        'PASSWORD': 'test',
         'HOST': 'localhost',
         'PORT': '3306',
     }
